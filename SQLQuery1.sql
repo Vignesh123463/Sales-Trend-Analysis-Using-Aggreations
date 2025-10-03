27632388a6c0c90d7e3eff99d0905421fab37f48
Vignesh k <Karuppannanvignesh10@gmail.com>
2025-10-03 21:59:18 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,119 @@
+create table student_s
+(
+id integer,
+product varchar(200),
+region varchar(200),
+salesperson varchar(200),
+quantity int,
+unitprice int,
+totalsales int
+)
+
+insert into student_s
+values(01,'mobile','north','vicky',4,16628,66512)
+insert into student_s
+values(02,'mouse','east','naveen',3,16628,66512)
+
+insert into student_s
+values(03,'mobile','west','nithiin',2,16628,66512)
+insert into student_s
+values(04,'camera','south','karthick',4,16628,66512)
+
+insert into student_s
+values(05,'mobile','east','santhosh',1,16628,66512)
+
+insert into student_s
+values(06,'printer','north','nitheesh',3,16628,66512)
+
+insert into student_s
+values(07,'mobile','west','ragul p',2,16628,66512)
+
+insert into student_s
+values(08,'laptop','south','ragul c',4,16628,66512)
+
+insert into student_s
+values(09,'mobile','north','nithin',1,16628,66512)
+
+insert into student_s
+values(10,'mouse','west','guhan',4,16628,66512)
+
+insert into student_s
+values(11,'mobile','south','rickson',3,16628,66512)
+insert into student_s
+values(12,'tablet','north','veera',4,16628,66512)
+
+insert into student_s
+values(01,'monitor','east','nagul',2,16628,66512)
+
+insert into student_s
+values(13,'mouse','west','naveen kumar',4,16628,66512)
+
+insert into student_s
+values(14,'camera','north','masthan',3,16628,66512)
+
+insert into student_s
+values(15,'mobile','south','salman',4,16628,66512)
+
+insert into student_s
+values(16,'printer','north','sriram',2,16628,66512)
+
+insert into student_s
+values(17,'headphones','east','nicklin',1,16628,66512)
+
+insert into student_s
+values(18,'smartwatch','west','rathish',3,16628,66512)
+
+insert into student_s
+values(19,'mobile','north','sham',4,16628,66512)
+
+insert into student_s
+values(20,'mouse','south','nimal',2,16628,66512)
+
+insert into student_s
+values(21,'laptop','east','nirmal kannan',3,16628,66512)
+
+insert into student_s
+values(22,'printer','north','richard',4,16628,66512)
+
+insert into student_s
+values(23,'camera','west','yuvan',1,16628,66512)
+
+insert into student_s
+values(24,'heartphones','south','venkadesh',3,16628,66512)
+
+insert into student_s
+values(25,'mobile','west','vimal',4,16628,66512)
+
+insert into student_s
+values(26,'phones','north','vignesh',2,16628,66512)
+
+insert into student_s
+values(27,'mobile','south','kabilan',3,16628,66512)
+
+insert into student_s
+values(28,'mouse','east','sriraj',4,16628,66512)
+
+insert into student_s
+values(29,'laptop','north','karuppusamy',3,16628,66512)
+
+insert into student_s
+values(30,'tablet','south','bukrandi',2,16628,66512)
+
+insert into student_s
+values(311,'smartwatch','west','kathir',1,16628,66512)
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
