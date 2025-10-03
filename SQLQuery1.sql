0cc2749af99f98cd45f0338b3c595bf475e87f79
Vignesh k <Karuppannanvignesh10@gmail.com>
2025-10-03 21:47:37 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,73 @@
+CREATE TABLE class
+(
+id integer,
+name varchar(255),
+age integer,
+gender varchar(255),
+city varchar(200)
+
+)
+INSERT INTO class
+VALUES(1,'karuppannan',45,'male','vairichettipalayam')
+INSERT INTO class
+VALUES(2,'chithra',40,'female','vairichettipalayam')
+Insert into class
+VALUES(3,'nadarajan',50,'male','vairichettipalayam')
+INSERT INTO class
+VALUES(4,'saroja',46,'female','vairichettipalayam')
+INSERT INTO class
+VALUES(1,'nagarajan',48,'male','vairichettipalayam')
+INSERT INTO class
+VALUES(1,'pichayi',43,'female','vairichettipalayam')
+INSERT INTO class
+VALUES(1,'kumar',33,'male','vairichettipalayam')
+INSERT INTO class
+VALUES(1,'selvi',31,'female','vairichettipalayam')
+INSERT INTO class
+VALUES(1,'sanjai',14,'male','vairichettipalayam')
+INSERT INTO class
+VALUES(1,'saravannan',33,'male','vairichettipalayam')
+INSERT INTO class
+VALUES(1,'rubiya',28,'female','vairichettipalayam')
+INSERT INTO class
+VALUES(1,'sathish',31,'female','vairichettipalayam')
+INSERT INTO class
+VALUES(1,'sowmiya',29,'female','vairichettipalayam')
+INSERT INTO class
+VALUES(1,'selvam',36,'male','uppiliyapuram')
+INSERT INTO class
+VALUES(1,'sudha',32,'female','uppiliyapuram')
+INSERT INTO class
+VALUES(1,'shalini',23,'female','uppiliyapuram')
+INSERT INTO class
+VALUES(1,'raju',42,'male','kollapatti')
+INSERT INTO class
+VALUES(1,'thangal',36,'female','kollapatti')
+INSERT INTO class
+VALUES(1,'logeshwaran',27,'male','kollapatti')
+INSERT INTO class
+VALUES(1,'monik',14,'male','kollapatti')
+INSERT INTO class
+VALUES(1,'manjunathan',36,'male','kollapatti')
+INSERT INTO class
+VALUES(1,'sundhari',31,'female','kollapatti')
+INSERT INTO class
+VALUES(1,'kishore',18,'male','kollapatti')
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
